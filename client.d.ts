37462671a6eca51ad124d57497abcf81d327cfe7
@Morelearn <morelearn1990@gmail.com>
2021-06-10 15:42:04 +0800

    fix: fix client.d.ts

--- client.d.ts
+++ client.d.ts
@@ -1,14 +1,17 @@
-import { History } from "history";
 declare module "virtual:generated-routes" {
+    import { History } from "history";
     import { ComponentType } from "react";
-    const RouteRoot: ComponentType;
+
     export interface RouteConfig {
         path: string;
         exact: boolean;
         component: ComponentType;
         routes?: RouteConfig[];
     }
+
     export type routes = RouteConfig[];
     export const history: History;
+
+    const RouteRoot: ComponentType;
     export default RouteRoot;
 }
